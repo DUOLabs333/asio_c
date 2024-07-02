8e1ea560954a5ff5059202dcc985562101757f0a
DUOLabs333 <dvdugo333@gmail.com>
2024-07-02 18:00:52 -0400

    Have first version of protocol to use

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <asio>
+#include <set>
+#include <string>
+#include <stdlib.h>
+
+std::set<int> free_sections;
+
+class Conn {
+	std::string DISK;
+	int section;
+	asio::
+
+	public:i
+	Conn(){
+		
+	}
+	void write(std::string){
+
+	}
+};
