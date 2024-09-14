96cbb682c5930509cf01c7febd4a7b9008431ffb
DUOLabs333 <dvdugo333@gmail.com>
2024-09-14 10:24:08 -0400

    Make some more progress

--- Server.cpp
+++ Server.cpp
@@ -35,7 +35,7 @@ int PORT=getEnv("CONN_SERVER_PORT", 4000);
 
 bool is_guest; 
 
-#define NUM_SEGMENTS 256
+#define NUM_SEGMENTS 256 //Must be no bigger than 256 (since the information is stored in a single byte
 typedef std::shared_ptr<socket_type> socket_ptr;
 
 socket_ptr server_connection = NULL; //TODO: Read constantly, if I disconnect, reexec (use argv for that)
@@ -50,11 +50,13 @@ typedef struct {
 	int fd = -1;
 	bool is_write; //Whether we will be writing to it (otherwise, we will be reading from it)
 	std::vector<SegmentInfo> segment_to_info;
-	char* mmap = NULL;
+	uint8_t* mmap = NULL;
+	size_t size;
 } DriveInfo;
 
 DriveInfo H2G, G2H; //Fill in path information in main()
 
+std::reference_wrapper<DriveInfo> Read = G2H, Write = H2G;
 
 auto SocketClose(socket_ptr& socket){
 	if (!socket){
@@ -69,6 +71,8 @@ auto SocketClose(socket_ptr& socket){
 typedef struct ThreadInfo{
 	socket_ptr conn = NULL;
 	uint32_t thread;
+	std::atomic<bool> connected = false;
+	std::mutex mu;
 } ThreadInfo;
 
 std::unordered_map<uint32_t, ThreadInfo> thread_to_info; //Map the client thread id to the corresponding thread. We use the client server thread number globally for both server and client (works as long as there is a 1-to-1 relationship between a client and server, and that upon disconnection on either side, we completely restart (ie, reexec
@@ -79,12 +83,45 @@ std::atomic<uint32_t> thread_counter = 0;
 
 asio::io_context context;
 
+std::mutex ring_mutex;
 /*
 You can't use ramdisks on macOS because mmap and pread/pwrite will fail --- since they depend on the size being non-zero. However, on macOS all disks has zero size (this is not true on Linux) --- so any offset argument has to be <= 0, which is a problem. You will take a performance hit (and may wear out the drive faster). Will try ivshmem to see if it minimizes memory copies (will need to patch QEMU, and will use mmap with msync --- can just memcpy)
 
-default tcp (even with macOS vmnet.framework specifically designed for VMs) is too slow. vhost-user, but want to minimize out-of-tree patches to QEMU (there is a patchset, but progress on it is moving very slowly), and more importantly, I have no idea how to use it (I asked a question in the mailing list and IRC, and have not recieved to either yet, at the time of writing this). Only supports Linux guests (macOS can not work with drives, and even ivshmem wouldn't work either)
+default tcp (even with the macOS-specific vmnet.framework specifically designed for VMs) is too slow. There is vhost-user, but want to minimize out-of-tree patches to QEMU (there is a patchset, but progress on it is moving very slowly), and more importantly, I have no idea how to use it (I asked a question in the mailing list and IRC, and have not received on either platform yet, at the time of writing this). Only supports Linux guests (macOS can not work with drives, and even ivshmem wouldn't work either on macOS)
 */
 
+auto WaitForChange(uint8_t* mem, std::function<bool(uint8_t,uint8_t)> f){
+	for(;;){
+		auto head = mem[0];
+		auto tail = mem[1];
+		if(f(head,tail)){
+			uint8_t length;
+
+			if(head == tail){
+				length = 0;
+			}else if(head < tail){
+				length = (tail-head)+1;
+			}else if(head > tail){
+				length = NUM_SEGMENTS-(head-tail)+1;
+			}
+
+			return std::make_tuple(head, length);
+		}
+
+		usleep(10);
+	}
+}
+
+void writeToRing(uint32_t thread, uint32_t msg_type, uint32_t arg1, socket_ptr conn = NULL, uint32_t size = 0){
+	std::unique_lock lk(ring_mutex);
+	
+	for(;;){
+		auto [ start, length ] = WaitForChange(Write.get().mmap, [](uint8_t a, uint8_t b){ return b!=(a-1);}); //Wait until the s
+
+	}
+
+	done:
+}
 void HandleConn(int key, ThreadInfo& info){ //Read from socket and write to ring
 //When quitting, remove from dictionary
 	std::array<uint8_t, 12> message_buf;
@@ -106,7 +143,7 @@ void HandleConn(int key, ThreadInfo& info){ //Read from socket and write to ring
 				{
 					auto backend = arg1;
 					writeToRing(info.thread, CONNECT, backend); //TODO: Locks mutex, waits until the tail!=head, serialize (thread, msg, arg1) to memory directly, and if ((conn != NULL) and (size > 0 )), write it out to segment (according to the size --- another thing, the size available to the 256 segments is shrunk by 2 for the head and tails), repeating across multiple segments as needed. After each write to segment update, if APPLE, then flush. After each write, make sure that there's space, then continue
-						info.connected.wait(false); //Atomic varible, waiting for an update
+						info.connected.wait(false); //Atomic variable, waiting for an update
 						writeToConn(*info.conn, message_buf, CONFIRM, 0, 0); //Tell UNIX socket that we've connected
 					break;		
 				}
@@ -143,36 +180,20 @@ void HandleConn(int key, ThreadInfo& info){ //Read from socket and write to ring
 }
 //0|1|2|3|4|5
 //T| |H|
-auto WaitForChange(uint8_t* mem, std::function<bool(uint8_t,uint8_t)> f){
-	for(;;){
-		auto head = mem[0];
-		auto tail = mem[1];
-		if(f(head,tail)){
-			uint8_t length = 0;
-
-			if(head == tail){
-				length = 0;
-			}else if(head < tail){
-				length = (tail-head)+1;
-			}else if(head > tail){
-				length = NUM_SEGMENTS-(head-tail)+1;
-			}
-
-			return std::make_tuple(head, length);
-		}
-
-		usleep(10);
-	}
-}
 void readFromRing(){
-	auto mem = Read.get().mem;
+	auto mem = Read.get().mmap;
 	//By the time the server accepts, and the client connects, the respective memory has been set to 0
+	auto& segments = Read.get().segment_to_info;
 
 	for(;;){
 		auto [start, length ] = WaitForChange(mem, [](uint8_t a, uint8_t b){ return a!=b;}); //Wait until ring buffer is not empty (as denoted by a==b)
+
 		for(int i =0; i< length; i++){
 			
-			auto [thread, msg_type, arg1] = deserializeInt(mem+start); //TODO: Should deserialize into three uint8_t
+			auto offset=segments[start].offset;
+			auto size = segments[start].size;
+
+			auto [thread, msg_type, arg1] = unpackMessage(mem+offset); //TODO: Should deserialize into three uint8_t
 	
 			t2i_mutex.lock_shared(); //Makes sure that checking + retrieving object is one atomic operation
 
@@ -191,8 +212,7 @@ void readFromRing(){
 					case(CONNECT): //Received request from client
 						{
 							info.thread = thread;
-							auto id = arg1;
-							auto& backend = get_backend(id);
+							auto& backend = get_backend(arg1);
 							connectToBackend(backend, info.conn); //TODO: Should move code from asio_c.cpp to utils 
 
 							writeToRing(thread, CONFIRM, 0);
@@ -202,9 +222,8 @@ void readFromRing(){
 
 					case(WRITE):
 						{
-						auto size = arg1;
-						writeToConn(*info.conn, WRITE, size, 0); //TODO: Make sure that asio_c is updated accordingly.
-						asio::write(*info.conn, asio::buffer(mem+start+12, size));
+						writeToConn(*info.conn, WRITE, arg1, 0); //TODO: Make sure that asio_c is updated accordingly.
+						asio::write(*info.conn, asio::buffer(mem+offset+12, size-12));
 
 						break;
 						}
@@ -227,25 +246,14 @@ void readFromRing(){
 	}
 }
 
-void FrontendServer(){
-	ip::tcp::acceptor acceptor(context, ip::tcp::endpoint(asio::ip::make_address(ADDRESS), PORT));
-       
-        for (;;){
-            auto socket=std::make_shared<socket_type>(context, TCP);
-            
-            acceptor.accept(*socket);
-
-	    socket->set_option(ip::tcp::no_delay( true)); //Should probably be placed in a wrapper function for making new tcp sockets (so I don't forget about the no_delay)
-
-
-            std::thread(HandleConn1, std::move(socket)).detach();
-        }
-}
 
 void HandleBackend(socket_ptr socket){
-	t2i_mutex.lock();
 	auto key = thread_counter++;
+
+	t2i_mutex.lock();
 	auto& info = thread_to_info[key];
+	t2i_mutex.unlock();
+
 	std::thread(HandleConn, key, std::ref(info)).detach();
 }
 void Server(){ //Only for client
@@ -287,6 +295,9 @@ int main(int argc, char** argv){
 	
 	is_guest = getEnv("CONN_SERVER_IS_GUEST", IS_GUEST_DEFAULT);
 	
+	if(is_guest){ //By default, we assumed we are on the host
+		std::swap(Read, Write);
+	}
 	H2G.file=getEnv("CONN_SERVER_H2G_FILE", H2G_DEFAULT_FILE);
 	G2H.file=getEnv("CONN_SERVER_G2H_FILE", G2H_DEFAULT_FILE);
 
@@ -355,7 +366,8 @@ int main(int argc, char** argv){
 
 	}
 		
-	
+	std::thread(readFromRing).detach();
+
 	asio::read(socket, asio::buffer(buf)); //As long as the client/server is alive, this should never return...
 	
 	execv(argv[0], argv); //...however, if it does, you should restart the whole program.
